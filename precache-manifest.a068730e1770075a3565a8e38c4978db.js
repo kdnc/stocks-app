ea94ac480925950cc1582908364a0d9d1ac6a8be
Nuwan Chamara <nuwanlanka@gmail.com>
2019-03-13 14:52:05 +0800

    Updates

---
+++ precache-manifest.a068730e1770075a3565a8e38c4978db.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "f16c0420cb27cb9be7df",
+    "url": "https://reduction-admin.github.io/react-reduction/static/css/main.60396b77.chunk.css"
+  },
+  {
+    "revision": "f16c0420cb27cb9be7df",
+    "url": "https://reduction-admin.github.io/react-reduction/static/js/main.33fc30a9.chunk.js"
+  },
+  {
+    "revision": "536ab3a6d4bd22e5559c",
+    "url": "https://reduction-admin.github.io/react-reduction/static/js/runtime~main.9983563b.js"
+  },
+  {
+    "revision": "ebb8c32d8997fef6b7b5",
+    "url": "https://reduction-admin.github.io/react-reduction/static/js/2.da6f1d89.chunk.js"
+  },
+  {
+    "revision": "80d4a4e5af9b6cf3882e4a34c5497542",
+    "url": "https://reduction-admin.github.io/react-reduction/static/media/sidebar-4.80d4a4e5.jpg"
+  },
+  {
+    "revision": "978e51b554db00a67e2796661b3526fe",
+    "url": "https://reduction-admin.github.io/react-reduction/static/media/100_4.978e51b5.jpg"
+  },
+  {
+    "revision": "b175e1c4790112fe609a0f3279e8535d",
+    "url": "https://reduction-admin.github.io/react-reduction/static/media/logo_200.b175e1c4.png"
+  },
+  {
+    "revision": "491902bb7c7bc96a8890997a83580211",
+    "url": "https://reduction-admin.github.io/react-reduction/index.html"
+  }
+];
